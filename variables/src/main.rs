ebf475f75b71a6367b61d1dbc1fb98c031647b63
Carlos Olivares <carlosolivares.331@outlook.com>
2022-09-18 07:34:53 -0700

    More example code forom chapter 3

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+const MAX_POINTS: u32 = 100_000;
+fn main() {
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("the value of x is: {}", x);
+    //let mut spaces = "";
+    //spaces = spaces.len()
+}
